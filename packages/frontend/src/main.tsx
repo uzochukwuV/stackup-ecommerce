1940902b1724edbd28cd9e4afe90c0e192c3cb04
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-09-20 23:44:02 +0800

    remove import of browser router

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store.ts";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
 	<StrictMode>
