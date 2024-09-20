ce991dc6c92107a91df3213a0990f25551a74a18
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-09-20 10:43:58 +0800

    update main.tsx

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -9,9 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root") as HTMLElement).render(
 	<StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
 				<App />
-			</BrowserRouter>
 		</Provider>
 	</StrictMode>,
 );
